94de4ec2eee50161af80f8bf6cfa92837506ada4
Chris Prather <chris@prather.org>
2010-05-23 22:03:42 -0400

    add transcode script

---
+++ bin/encode_video.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+INPUT=$1
+NAME=`echo $INPUT | cut -d\. -f1`
+
+echo 'Making output directory'
+mkdir -p ${NAME};
+
+echo 'Making h.264 video'
+ffmpeg -i ${INPUT} -acodec libfaac -ab 128k -vcodec libx264 -vpre slow -crf 22 -threads 0 -s 440x224 ${NAME}/${NAME}.mp4
+
+echo 'Making Ogg/Theora video'
+ffmpeg2theora --videoquality 5 --audioquality 1 --max_size 440x224 -o ${NAME}/${NAME}.ogv ${INPUT} 
+
+echo 'Making Thumbnail Now'
+ffmpeg  -itsoffset -4  -i ${INPUT} -vcodec mjpeg -vframes 1 -an -f rawvideo -s 320x240 ${NAME}/thumb.jpg
+
