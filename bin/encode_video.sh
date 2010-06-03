247bb070a821879156031c495c0bf9f88b8dcd0c
Chris Prather <chris@prather.org>
2010-06-03 05:40:52 -0400

    tweak the max_size

--- bin/encode_video.sh
+++ bin/encode_video.sh
@@ -6,11 +6,11 @@ echo 'Making output directory'
 mkdir -p ${NAME};
 
 echo 'Making h.264 video'
-ffmpeg -i ${INPUT} -acodec libfaac -ab 128k -vcodec libx264 -vpre slow -crf 22 -threads 0 -s 440x224 ${NAME}/${NAME}.mp4
+ffmpeg -i ${INPUT} -acodec libfaac -ab 128k -vcodec libx264 -vpre slow -crf 22 -threads 0 -s 640x480 ${NAME}/${NAME}.mp4
 
 echo 'Making Ogg/Theora video'
-ffmpeg2theora --videoquality 5 --audioquality 1 --max_size 440x224 -o ${NAME}/${NAME}.ogv ${INPUT} 
+ffmpeg2theora --videoquality 5 --audioquality 1 --max_size 640x480 -o ${NAME}/${NAME}.ogv ${INPUT} 
 
 echo 'Making Thumbnail Now'
-ffmpeg  -itsoffset -4  -i ${INPUT} -vcodec mjpeg -vframes 1 -an -f rawvideo -s 320x240 ${NAME}/thumb.jpg
+ffmpeg  -itsoffset -4  -i ${INPUT} -vcodec mjpeg -vframes 1 -an -f rawvideo -s 640x480 ${NAME}/thumb.jpg
 
